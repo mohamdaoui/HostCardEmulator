f6597ece15344ffcdec9fc8c8a0f7e57138c7dc0
Mohamdaoui <hamdaouismohamed@gmail.com>
2017-11-02 10:31:24 -0400

    Initial Commit

---
+++ app/src/main/java/com/mhamdaoui/hostcardemulator/HostCardEmulatorService.kt
@@ -0,0 +1,52 @@
+package com.mhamdaoui.hostcardemulator
+
+import android.nfc.cardemulation.HostApduService
+import android.os.Bundle
+import android.util.Log
+
+/**
+ * Created by mhamdaoui on 2017-10-27.
+ */
+class HostCardEmulatorService: HostApduService() {
+
+    companion object {
+        val TAG = "Host Card Emulator"
+        val STATUS_SUCCESS = "9000"
+        val STATUS_FAILED = "6F00"
+        val CLA_NOT_SUPPORTED = "6E00"
+        val INS_NOT_SUPPORTED = "6D00"
+        val AID = "A0000002471001"
+        val SELECT_INS = "A4"
+        val DEFAULT_CLA = "00"
+        val MIN_APDU_LENGTH = 12
+    }
+
+    override fun onDeactivated(reason: Int) {
+        Log.d(TAG, "Deactivated: " + reason)
+    }
+
+    override fun processCommandApdu(commandApdu: ByteArray?, extras: Bundle?): ByteArray {
+        if (commandApdu == null) {
+            return Utils.hexStringToByteArray(STATUS_FAILED)
+        }
+
+        val hexCommandApdu = Utils.toHex(commandApdu)
+        if (hexCommandApdu.length < MIN_APDU_LENGTH) {
+            return Utils.hexStringToByteArray(STATUS_FAILED)
+        }
+
+        if (hexCommandApdu.substring(0, 2) != DEFAULT_CLA) {
+            return Utils.hexStringToByteArray(CLA_NOT_SUPPORTED)
+        }
+
+        if (hexCommandApdu.substring(2, 4) != SELECT_INS) {
+            return Utils.hexStringToByteArray(INS_NOT_SUPPORTED)
+        }
+
+        if (hexCommandApdu.substring(10, 24) == AID)  {
+            return Utils.hexStringToByteArray(STATUS_SUCCESS)
+        } else {
+            return Utils.hexStringToByteArray(STATUS_FAILED)
+        }
+    }
+}
---
+++ app/src/main/java/com/mhamdaoui/hostcardemulator/Utils.kt
@@ -0,0 +1,40 @@
+package com.mhamdaoui.hostcardemulator
+
+/**
+ * Created by mhamdaoui on 2017-10-27.
+ */
+class Utils {
+
+    companion object {
+        private val HEX_CHARS = "0123456789ABCDEF"
+        fun hexStringToByteArray(data: String) : ByteArray {
+
+            val result = ByteArray(data.length / 2)
+
+            for (i in 0 until data.length step 2) {
+                val firstIndex = HEX_CHARS.indexOf(data[i]);
+                val secondIndex = HEX_CHARS.indexOf(data[i + 1]);
+
+                val octet = firstIndex.shl(4).or(secondIndex)
+                result.set(i.shr(1), octet.toByte())
+            }
+
+            return result
+        }
+
+        private val HEX_CHARS_ARRAY = "0123456789ABCDEF".toCharArray()
+        fun toHex(byteArray: ByteArray) : String {
+            val result = StringBuffer()
+
+            byteArray.forEach {
+                val octet = it.toInt()
+                val firstIndex = (octet and 0xF0).ushr(4)
+                val secondIndex = octet and 0x0F
+                result.append(HEX_CHARS_ARRAY[firstIndex])
+                result.append(HEX_CHARS_ARRAY[secondIndex])
+            }
+
+            return result.toString()
+        }
+    }
+}
